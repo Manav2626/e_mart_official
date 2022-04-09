8374f3885689138f4f36ba78e6ac3aa675c10ca8
Manav2626 <manavwork01@gmail.com>
2022-04-09 11:19:08 +0530

    commit

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,61 @@
+import 'package:e_mart/home/food_page_body.dart';
+import 'package:e_mart/utils/colors.dart';
+import 'package:e_mart/widgets/big_text.dart';
+import 'package:e_mart/widgets/small_text.dart';
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+
+        child: Container(
+          margin: EdgeInsets.only(top:45, bottom: 15),
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  BigText(text: "India", color: Appcolors.mainColor),
+                  Row(
+                    children: [
+                      SmallText(text: "City", color: Colors.black54,),
+                      Icon(Icons.arrow_drop_down_rounded)
+                    ],
+                  )
+                ],
+              ),
+              Center(
+                child: Container(
+                  width: 45,
+                  height: 45,
+                  child: Icon(Icons.search, color: Colors.white,),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Appcolors.mainColor,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+
+      FoodPageBody(),
+    
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/icon_and_text.dart
@@ -0,0 +1,33 @@
+import 'package:e_mart/widgets/small_text.dart';
+import 'package:flutter/cupertino.dart';
+
+class IconAndTExtWidget extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  final Color iconColor;
+  const IconAndTExtWidget(
+      {Key? key,
+      required this.icon,
+      required this.text,
+      required this.iconColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: iconColor,
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        SmallText(
+          text: text,
+        ),
+      ],
+    );
+  }
+}
