22904acfe26f717e67abc0a733b787c499ef290e
Manav2626 <manavwork01@gmail.com>
2022-04-17 17:24:16 +0530

    rewrite popular controller done

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -2,29 +2,17 @@ import 'package:e_mart/data/repository/popular_product_repo.dart';
 import 'package:get/get_connect/http/src/response/response.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
-import '../models/products_model.dart';
-
-class PopularProductController extends GetxService {
+class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
   PopularProductController({required this.popularProductRepo});
   List<dynamic> _popularProductList = [];
   List<dynamic> get popularProductList => _popularProductList;
-
-  bool _isLoaded = false;
-  bool get isLoaded => _isLoaded;
-
-  Future<void> getPopularPrductList() async {
+  Future<void> getpopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
-      print("got products");
       _popularProductList = [];
-      _popularProductList.addAll(Product.fromJson(response.body).products);
-      //print(_popularProductList);
-      _isLoaded = true;
+      // _popularProductList.addAll();
       update();
     } else {}
   }
-
-//aa nichenu error kadhva add karyu chh
-  void update() {}
 }
